548d4bfbb058974d03a1b0b86a3a15ffb80197ff
Christian Dupuis <cd@atomist.com>
2019-11-18 14:07:20 +0100

    Add secrets

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2020111 Atomist, Inc.  
+ * Copyright © 2020112s1 Atomist, Inc.  
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.   
