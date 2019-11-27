04dea44f598b79af2ec85772ce0d5efcf4eaafe3
Christian Dupuis <cd@atomist.com>
2019-11-27 11:31:29 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@
  * You may obtain a copy of the License at  
  *      
  *     http://www.apache.org/licenses/LICENSE-2.0          
- *      
+ *       
  * Unless required by applicable law or agreed to in w riting, software  
  * distributed under the License i s distributed on an "AS IS" BASIS,  
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   
