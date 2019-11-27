73df0f1c43539261c4ee93f0c8f7d62b5f96020d
Christian Dupuis <cd@atomist.com>
2019-11-27 11:38:50 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.   
  * You may obtain a copy of the License at  
  *       
- *     http://www.apache.org/licenses/LICENSE-2.0           
+ *     http://www.apache.org/licenses/LICENSE-2.0            
  *       
  * Unless required by applicable law or agreed to in w riting, software  
  * distributed under the License i s distributed on an "AS IS" BASIS,  
